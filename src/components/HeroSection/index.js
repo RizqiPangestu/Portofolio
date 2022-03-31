46c09644ef1faa16c5af8c60ee76b8c518d42d4c
rizqipangestu <rizqipangestu@mail.ugm.ac.id>
2022-04-01 00:30:03 +0700

    add greeting content

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP } from './HeroElements'
 import Video from '../../videos/video.mp4'
 
 const HeroSection = () => {
   return (
       <HeroContainer>
           <HeroBg>
-              <VideoBg autoPlay loop muted src={ Video } type='videp/mp4'/>
+              <VideoBg autoPlay loop muted src={Video} type='videp/mp4' />
+              <div>test</div>
           </HeroBg>
+          <HeroContent>
+              <HeroH1>Hello, I'm Estu</HeroH1>
+              <HeroP>
+                  I'm a Computer Science graduate from
+                  Universitas Gadjah Mada who passionate
+                  about backend development and
+                  computer vision
+              </HeroP>
+          </HeroContent>
       </HeroContainer>
   )
 }
